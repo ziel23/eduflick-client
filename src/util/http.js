610f4cbf32b3b9f3d9beb7878a80f53bc0e05908
Jhun Wulf Sabala <sabala.jw@nec.com>
2025-03-12 19:51:17 +0800

    http updatE

--- src/util/http.js
+++ src/util/http.js
@@ -2,27 +2,26 @@ import axios from 'axios';
 
 // Create an Axios instance
 const axiosInstance = axios.create({
-  // baseURL: 'http://localhost:8000/api/', // Replace with your API base URL
-  baseURL: 'https://eduflickserver.vercel.app/api/',
+  baseURL: 'https://eduflickserver.vercel.app/api/', // Update with your actual API URL
   withCredentials: true, // Important for authentication (cookies, JWT)
-
+  headers: {
+    "Content-Type": "application/json"
+  }
 });
 
 // Add a request interceptor
 axiosInstance.interceptors.request.use(
   config => {
     const token = localStorage.getItem('token');
-    if (config.url !== "auth/login" && config.url !== "auth/register") {
-      config.headers.Authorization = `${token}`;
+
+    if (token && !config.url.includes("auth/login") && !config.url.includes("auth/register")) {
+      config.headers.Authorization = `Bearer ${token}`;
     }
-    config.headers["Content-Type"] = "application/json";
 
     console.log('Request Interceptor:', config);
     return config;
   },
-  error => {
-    return Promise.reject(error);
-  }
+  error => Promise.reject(error)
 );
 
 // Add a response interceptor
@@ -37,7 +36,7 @@ axiosInstance.interceptors.response.use(
     if (error.response?.status === 401) {
       localStorage.removeItem("token"); // Clear token if unauthorized
       window.location.href = "/"; // Redirect to login page
-      return {}
+      return Promise.reject({ message: "Unauthorized" });
     }
 
     return Promise.reject(error);
